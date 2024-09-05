c2f6840a64dd837ffcfe2cf000b8257d4c9fc85e
Purnima Madhiraju <purnima.madhiraju@veeva.com>
2024-09-05 16:10:54 +0530

    Created a class to find sum of 2 numbers

---
+++ Sum.java
@@ -0,0 +1,11 @@
+public class Sum {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        int a,b;
+        a=5;
+        b=10;
+        int c=a+b;
+        System.out.println(c);
+    }
+
+}
